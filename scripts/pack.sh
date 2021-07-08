b498a791f090a542841c7ebeb3f4d202df7d8186
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2021-07-08 19:03:13 +0200

    updated pack

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -2,12 +2,12 @@
 echo Executing after success scripts on branch $TRAVIS_BRANCH
 echo Triggering NuGet package build
 
-dotnet pack -p:NuspecFile=.\Genocs.Integration.MSAzure.nuspec --no-restore -o .
+dotnet pack .\src\Genocs.Integration.ML.CognitiveServices\Genocs.Integration.ML.CognitiveServices.csproj -o . -p:Version=1.0.$TRAVIS_BUILD_NUMBER
 
-echo Uploading Genocs.Integration.MSAzure package to NuGet using branch $TRAVIS_BRANCH
+echo Uploading Genocs.Integration.ML.CognitiveServices package to NuGet using branch $TRAVIS_BRANCH
 
 case "$TRAVIS_BRANCH" in
   "master")
     dotnet nuget push *.nupkg -k $NUGET_API_KEY -s $NUGET_SOURCE
-    ;;  
+    ;;
 esac
