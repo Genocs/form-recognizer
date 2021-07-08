adbdc696d8c0c57200ff61a121cd0c8f93a7bb2c
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2021-07-08 19:14:57 +0200

    Fix path

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -2,7 +2,7 @@
 echo Executing after success scripts on branch $TRAVIS_BRANCH
 echo Triggering NuGet package build
 
-dotnet pack .\src\Genocs.Integration.ML.CognitiveServices\Genocs.Integration.ML.CognitiveServices.csproj -o . -p:Version=1.0.$TRAVIS_BUILD_NUMBER
+dotnet pack ./src/Genocs.Integration.ML.CognitiveServices/Genocs.Integration.ML.CognitiveServices.csproj -o . -p:Version=1.0.$TRAVIS_BUILD_NUMBER
 
 echo Uploading Genocs.Integration.ML.CognitiveServices package to NuGet using branch $TRAVIS_BRANCH
 
