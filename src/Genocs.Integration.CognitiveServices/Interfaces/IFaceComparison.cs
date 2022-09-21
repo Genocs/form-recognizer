c01858bf9d357b2079ae7aafd1db9fde8ed122fa
Giovanni Emanuale Nocco <giovanni.nocco@gmail.com>
2022-09-21 22:04:00 +0200

    Removed face comparison

--- src/Genocs.Integration.CognitiveServices/Interfaces/IFaceComparison.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
-
-namespace Genocs.Integration.CognitiveServices.Interfaces;
-
-public interface IFaceComparison
-{
-    Task<IList<SimilarFace>> FindSimilar(string firstImage, string secondImage);
-}
